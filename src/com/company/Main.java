e4bab738ada294fa9c5354ee4762a34f0a319444
纪智焓 <jizh@entrobus.com>
2019-10-25 17:57:21 +0800

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String action = test();
+        //
+    }
+
+    static String test(){
+        return "0";
+    }
+}
